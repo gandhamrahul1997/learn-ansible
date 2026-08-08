58fdae804cb885cb0ad91cd6dabcc539240ec81e
Rahul1971 <rahulgandham1997@gmail.com>
2026-08-08 14:11:13 +0530

    Initial commit

--- ansible/testfile.sh
+++ ansible/testfile.sh
@@ -1,7 +1,6 @@
-echo "Number of passed arguments are:" $#
-echo "Passed arguments are:" $@
-echo "Number of passed arguments are full line:" $*
-echo "Return code of above command:" $?
-echo "pid of above command:" $$
+echo "Number of passed arguments are:" "$#"
+echo "Passed arguments are:" "$@"
+echo "Number of passed arguments are full line:" "$*"
+echo "pid of above command:" "$$"
 
-echo "Run completed"
+echo "Run completed today"
